703b6f17a9b6a1f7fab5d1c1a36c7b26063f4660
Ryanhtech Labs <hello.ryanhtech.labs+git@gmail.com>
2026-06-05 23:18:59 +0200

    Add command-line argument parser and PDF save directory initialisation

--- trayner.py
+++ trayner.py
@@ -15,9 +15,77 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import argparse
+import os
+import sys
+
+
+def get_default_pdf_directory_absolute_path() -> str:
+    """
+    Returns the absolute path to the default PDF save directory.
+    The default directory is the 'pdf' directory located in this script's directory. Please be aware that this directory
+    might not be writeable.
+    :return: the absolute path to the default PDF save directory.
+    """
+    # Use the os module to determine the path to the script's directory, and add the default PDF save directory name
+    # which is obviously 'pdf'
+    script_directory_path = os.path.abspath(os.path.dirname(__file__))
+
+    # Thank you very much Micro$oft
+    if sys.platform == "win32":
+        return script_directory_path + "\\pdf"
+    return script_directory_path + "/pdf"
+
+def init_pdf_directory(pdf_dir_path: str):
+    """
+    Initialises the PDF directory. If it doesn't exist, this function creates it.
+    :raise IOError: If the directory isn't writeable or if it cannot be created.
+    :raise FileExistsError: If the provided path is a file.
+    :param pdf_dir_path: The absolute path to the PDF directory.
+    """
+    if os.path.isfile(pdf_dir_path):
+        # The file is not a directory
+        raise FileExistsError(f"The file '{pdf_dir_path}' is not a directory")
+    elif os.path.isdir(pdf_dir_path):
+        # Check if the directory is writeable (write and execute permissions)
+        if not os.access(pdf_dir_path, os.W_OK | os.X_OK):
+            raise IOError(f"The directory '{pdf_dir_path}' doesn't seem to be writeable")
+        return
+
+    # The directory doesn't exist; create it
+    try:
+        os.mkdir(pdf_dir_path)
+    except:
+        raise IOError(f"The directory '{pdf_dir_path}' couldn't be created.")
+
+def init_parse_arguments() -> argparse.Namespace:
+    """
+    Parses the command-line arguments that were provided to the programme.
+    :return: The parsed command-line arguments.
+    """
+    # Create an ArgumentParser object that we'll populate with the supported command-line arguments
+    argument_parser = argparse.ArgumentParser(prog="trayner")
+    argument_parser.add_argument("--pdf_directory", default=get_default_pdf_directory_absolute_path())
+
+    # Return the parsed arguments
+    return argument_parser.parse_args()
+
 def main():
-    # TODO main routine
-    pass
+    """
+    Main routine.
+    :return: exit code
+    """
+    # Parse the command-line arguments
+    arguments = init_parse_arguments()
+
+    # Initialise the PDF directory
+    init_pdf_directory(arguments.pdf_directory)
+
+    return 0
 
 if __name__ == "__main__":
-    main()
+    # Execute the main routine, and get its exit code
+    exit_code = main()
+
+    # Exit with the specified exit code
+    sys.exit(exit_code)
